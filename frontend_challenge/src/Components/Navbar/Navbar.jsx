587d9aae066c68015438a1230206503163f9d08b
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-19 18:18:21 +0300

    Frontend:working on navbar styling

--- frontend_challenge/src/Components/Navbar/Navbar.jsx
+++ frontend_challenge/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
 import "./navbar.css"
-
+import { Link } from 'react-router-dom';
+import {FaBars} from 'react-icons/fa'
 const Navbar = () => {
   return (
    <div className="navbar-wrapper">
       <div className="navbar_container">
-        <div className="logo">A11yMastery</div>
+          <div className="logo">A11yMastery</div>
+          <div className="nav-icon">
+            <FaBars/>
+          </div>
+          <div className="nav_menu">
+            <div className="nav_items">
+              <Link to="about" className="nav_links">About</Link>
+              <Link to="services" className="nav_links">Services</Link>
+              <Link to="discover" className="nav_links">Discover</Link>
+              <Link to="signup" className="nav_links">Sign Up</Link>
+            </div>
+          </div>
       </div>
     </div>
   )
