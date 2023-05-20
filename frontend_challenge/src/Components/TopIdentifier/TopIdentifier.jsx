48a5147799c6c3d18d6a4183ed6a4434813c756f
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-20 19:56:46 +0300

    Frontend:creating top identifier :art:

---
+++ frontend_challenge/src/Components/TopIdentifier/TopIdentifier.jsx
@@ -0,0 +1,13 @@
+import "./topidentifier.css"
+
+const TopIdentifier = ({title}) => {
+  return (
+    <div className="top-section">
+        <div className="top-title">
+            <h1 className='top-name'>{title}</h1>
+        </div>
+    </div>
+  )
+}
+
+export default TopIdentifier
