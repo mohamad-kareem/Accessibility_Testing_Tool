b1cd565c8e43915150773637dea29cd7b417e66a
Mohamad Karim <mohamadkareemeng@gmail.com>
2023-05-22 00:37:07 +0300

    Frontend:implementing the structure of my table :hammer:

---
+++ frontend_challenge/src/Components/InfoTable/InfoTable.jsx
@@ -0,0 +1,25 @@
+import "./infotable.css"
+
+const InfoTable = () => {
+  return (
+  <div className="table-container">
+    <div className="table-content">
+        <div className="section-one">
+            <h2></h2>
+            <p></p>
+        </div>
+        <div className="section-two">
+            <h2></h2>
+            <p></p>
+        </div>
+        <div className="section-three">
+            <h2></h2>
+            <p></p>
+        </div>
+    </div>
+    
+  </div>
+  )
+}
+
+export default InfoTable
